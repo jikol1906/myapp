e9b6b011c36dcea1b8140867cbdddcbb089f3259
Boris Grunwald <borisgrunwald@gmail.com>
2017-08-25 12:44:37 +0200

    initial

---
+++ Hello.js
@@ -0,0 +1,14 @@
+//Webserver
+
+var http = require('http');
+
+var server = http.createServer(function(req,res){
+    res.writeHead(200,{'Content-type' : 'text/html'});
+    res.write('<h1 align="center">:0)</h1>');
+    res.end();
+});
+
+
+server.listen(3003);
+
+
